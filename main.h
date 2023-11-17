9e1149568b38cd9cee55f1abe9f968e6e30ce02e
root <you@example.com>
2023-11-17 07:05:00 -0800

    first

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <limits.h>
 #include <stdarg.h>
 #include <unistd.h> 
 #include <stdlib.h>
 
+int main_putchar(char c);
 int _printf(const char *format, ...);
 int putss(char *c);
-int main_putchar(char c);
+
 #endif
--- printf.c
+++ printf.c
@@ -42,3 +42,18 @@ int _printf(const char *format, ...) {
     return (count);
 }
 
+#include "main.h"
+
+/**
+*  main_putchar
+* @c: char input
+*
+*
+Return: 1
+*/
+
+int main_putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
